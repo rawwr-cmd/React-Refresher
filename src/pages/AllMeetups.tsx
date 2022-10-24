3893e99066621cb3f0c3525f65bca53e95ddf34f
rawwr <akshayace0302@gmail.com>
2022-10-24 19:29:40 +0530

    using useContext

--- src/pages/AllMeetups.tsx
+++ src/pages/AllMeetups.tsx
@@ -1,29 +1,39 @@
+import { useState, useEffect } from "react";
 import MeetUpList from "../components/meetups/MeetupList";
 import { Meetup } from "../meetup.modal";
-const dummyData: Meetup[] = [
-  {
-    id: Math.random().toString(),
-    title: "This is a first meetup",
-    image:
-      "https://images.unsplash.com/photo-1666479245242-1e4a4d516540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=407&q=80",
-    address: "Some address 5, 12345 Some City",
-    description: "first meetup!",
-  },
-  {
-    id: Math.random().toString(),
-    title: "This is a second meetup",
-    image:
-      "https://images.unsplash.com/photo-1664575197229-3bbebc281874?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    address: "Some address 5, 12345 Some City",
-    description: "second meetup!",
-  },
-];
 
 const AllMeetupsPage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [loadedMeetups, setLoadedMeetups] = useState<Meetup[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-refresher-90d33-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const meetups: Meetup[] = [];
+        for (const key in data) {
+          const meetup: Meetup = {
+            id: key,
+            ...data[key], //copying key-value pairs
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    <section>Loading...</section>;
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetUpList meetups={dummyData} />
+      <MeetUpList meetups={loadedMeetups} />
     </section>
   );
 };
--- src/pages/NewMeetups.tsx
+++ src/pages/NewMeetups.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 const NewMeetupsPage: React.FC = () => {
-  return <div>New Meetups Page</div>;
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData: {}) => {
+    fetch(
+      "https://react-refresher-90d33-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/"); // navigate back to home page
+    });
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetupsPage;
